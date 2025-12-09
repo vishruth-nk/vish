7789875230b796c5541a68b6abb735098cf9a990
vishruth-nk <vishpandu000@gmail.com>
2025-12-09 06:29:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // 👇 IMPORTANT for GitHub Pages project site
-  // repo name = SSSBlov, so base should be /SSSBlov/
-  base: "/vish/",
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
-
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/vish/', // 👈 repo name
+  plugins: [react()],
+})
