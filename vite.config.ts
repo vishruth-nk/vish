aa273731a7fc9bbc58435e35b7f81d14a7d987ae
vishruth-nk <vishpandu000@gmail.com>
2025-12-09 07:24:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
-  base: '/vish/', // 👈 repo name
+  base: "/vish/", // repo name for GitHub Pages
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 👈 so "@/components/..." works
+    },
+  },
+});
